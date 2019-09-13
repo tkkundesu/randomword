03c5cb99399410d19a72906f84b52dfeaa5cf06d
tkkundesu <54850739+tkkundesu@users.noreply.github.com>
2019-09-13 15:09:10 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php session_start()?>
+<?php
+require_once(__DIR__.'/config.php');
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="utf-8">
+	<title>wordrandom</title>
+	<link rel="stylesheet" href="styles.css">
+	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+      <script src="app.js"></script>
+
+</head>
+<body>
+ <header>
+ 	<div class="top">
+	<p><a href="edit.php">編集</a></p>
+	<p><a href="customer-input.php">新規登録</a></p>
+	<p><a href="login.php">ログイン</a></p>
+	<p><a href="logout.php">ログアウト</a></p>
+	<?php
+	if(isset($_SESSION['customer'])){
+		echo '<p>ようこそ<span class="user-name">'.$_SESSION['customer']['name'].'</span>さん。</p>';
+	}else{
+		echo '<p>ようこそゲストさん。</p>';
+	}?>
+    </div>
+ </header>
+ <main>
+ 	<div class="container">
+ 	<h1>ワードガチャ</h1>
+ 	<form action="result.php" method="post">
+ 		<button><a href="result.php">START!!</a></button>
+ 	</form>
+ 	</div>
+ </main>
+</body>
+</html>
+
