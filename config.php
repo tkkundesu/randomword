a68a18c74a387bbe158a353e496c977121e63162
tkkundesu <54850739+tkkundesu@users.noreply.github.com>
2019-09-13 15:12:09 +0900

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+ini_set('display_errors', 1);
+
+define('DSN','mysql:host=localhost;dbname=list;charset=utf8');
+define('DB_USERNAME','dbuser');
+define('DB_PASSWORD','BtFEpSSvdhRTTwCx');
+
+require_once(__DIR__.'/function.php');
+?>
