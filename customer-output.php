70cc84c9699db94ef07d42c0dd64571a7e6c889d
tkkundesu <54850739+tkkundesu@users.noreply.github.com>
2019-09-27 14:29:40 +0900

    Update customer-output.php

--- customer-output.php
+++ customer-output.php
@@ -3,33 +3,33 @@
 require_once(__DIR__.'/config.php');
 
 try{
-	$db=new PDO(DSN,DB_USERNAME,DB_PASSWORD);
+	$db=new PDO(DSN,DB_USERNAME,DB_PASSWORD);//データベース接続
     $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     
    
 
  }catch(PDOException $e){
-     echo $e->getmessage();
+     echo $e->getmessage();//例外表示
      exit;
  }
  if(isset($_SESSION["customer"])){
   $id=$_SESSION["customer"]["id"];
-  $sql=$db->prepare('select * from customer where id!=? and login=?');
+  $sql=$db->prepare('select * from customer where id!=? and login=?');//ログイン状態のログイン名更新時の重複チェック
   $sql->execute([$id,$_REQUEST['login']]);
 }else{
-  $sql=$db->prepare('select * from customer where login=?');
+  $sql=$db->prepare('select * from customer where login=?');//新規登録時のログイン名重複チェック
   $sql->execute([$_REQUEST['login']]);
 }
 if(empty($sql->fetch())){
   if(isset($_SESSION["customer"])){
-    $sql=$db->prepare('update customer set name=?,login=?,password=? where id=?');
+    $sql=$db->prepare('update customer set name=?,login=?,password=? where id=?');//ログイン状態時の会員情報をデータベースへアップデート
     $sql->execute([$_REQUEST["name"],$_REQUEST["login"],$_REQUEST["password"],$id]);
     $_SESSION["customer"]=[
-    'id'=>$id,'name'=>$_REQUEST['name'],'login'=>$_REQUEST['login'],
+    'id'=>$id,'name'=>$_REQUEST['name'],'login'=>$_REQUEST['login'],//セッションに格納
     'password'=>$_REQUEST['password']];
      echo '<script>alert("お客様情報を更新しました。");</script>';
   } else {
-  $sql=$db->prepare('insert into customer values(null,?,?,?)');
+  $sql=$db->prepare('insert into customer values(null,?,?,?)');//新規顧客情報をデータベースへ追加
   $sql->execute([$_REQUEST["name"],$_REQUEST["login"],$_REQUEST["password"]]);
    echo '<script>alert("お客様情報を登録しました。");</script>';
 
