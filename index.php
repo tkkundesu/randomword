6969312640619b06ebc7d262769839ae1d646ba0
tkkundesu <54850739+tkkundesu@users.noreply.github.com>
2019-09-30 18:51:53 +0900

    Update index.php

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ require_once(__DIR__.'/config.php');
  <header>
  	<div class="top">
 	<p><a href="edit.php">編集</a></p>
+	<p><a href="customer-input.php">
 <?php
   //ログイン状態では会員登録変更
 if(isset($_SESSION['customer'])){
